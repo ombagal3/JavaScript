1007b5615615e37bbf56cc1edd4de49abc81ccdb
Om Bagal <ombagal8@gmail.com>
2025-12-01 23:06:30 +0530

    Add files via upload

---
+++ Day 18/script.js
@@ -0,0 +1,10 @@
+const input = document .getElementById('input');
+const button = document .getElementById('btn');
+const p1 = document .getElementById('p1');
+const output = document .getElementsByClassName('output');
+
+button.addEventListener("click", () => {
+     const value = input.value;
+    p1.textContent = value;
+    // output .textContent = `Output: ${text .toUpperCase()}`;
+});
