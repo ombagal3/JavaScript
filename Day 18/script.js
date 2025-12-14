4898793a540e476423930b5802284f05a9cbb0c6
Om Bagal <ombagal8@gmail.com>
2025-12-14 19:24:39 +0530

    Refactor event listener setup in script.js

--- Day 18/script.js
+++ Day 18/script.js
@@ -4,7 +4,11 @@ const p1 = document .getElementById('p1');
 const output = document .getElementsByClassName('output');
 
 button.addEventListener("click", () => {
+
+
+     
      const value = input.value;
     p1.textContent = value;
     // output .textContent = `Output: ${text .toUpperCase()}`;
+
 });
