44f9fecdb80bda8f9014d39bb4a3163f8ece084d
Om Bagal <ombagal8@gmail.com>
2025-12-15 15:10:49 +0530

    Add files via upload

---
+++ chapter-4switch while loop/switch case statements/script.js
@@ -0,0 +1,17 @@
+let choice ="b";
+switch(choice){
+    case "A":
+        console.log("wrong answer");
+        break;
+        case "B":
+        case "b":
+        console.log("Right Ans");
+        break;
+        case "C":
+        console.log("Wrong ans");
+        break;
+        case "D":
+        console.log("wrong ans");
+        default:
+            console.log("not valid");
+}
