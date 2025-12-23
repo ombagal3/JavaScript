37dd07c9885db36ffba1f09ebb0c45df162eb542
Om Bagal <ombagal8@gmail.com>
2025-12-23 23:19:06 +0530

    Update script.js

--- pr-3/script.js
+++ pr-3/script.js
@@ -52,6 +52,8 @@
 // let result = minArray(arr);
 // document.writeln(result);
 
+
+
 // 4.Write a function that takes an array and returns the average of all numbers.
 // let arr=[10,20,2,50,60];
 // let sum=0;
@@ -522,6 +524,7 @@
 // ];
 
 // console.log(totalQuantity(cart));
+
 
 
 
