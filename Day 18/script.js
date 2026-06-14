e0ccbfe6bd5ae16363890d27005f2e5b5e1eb77f
Om Bagal <ombagal8@gmail.com>
2026-06-14 11:45:19 +0530

    Update script.js

--- Day 18/script.js
+++ Day 18/script.js
@@ -5,7 +5,6 @@ const output = document .getElementsByClassName('output');
 
 button.addEventListener("click", () => {
   
-
   const value = input.value;
   p1.textContent = value;
     // output .textContent = `Output: ${text .toUpperCase()}`;
