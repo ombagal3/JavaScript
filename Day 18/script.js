3b2f927b028b4ff853ccac4a7e996059c4491081
Om Bagal <ombagal8@gmail.com>
2026-07-12 23:00:39 +0530

    Update script.js

--- Day 18/script.js
+++ Day 18/script.js
@@ -2,7 +2,7 @@ const input = document .getElementById('input');
 const button = document .getElementById('btn');
 const p1 = document .getElementById('p1');
 const output = document .getElementsByClassName('output'); 
-
+ 
 button.addEventListener("click", () => {  
     
   const value = input.value; 
